09fd485583348b7b3cd72c5d5b3dd56fb5078038
Sondro <memoiran@hotmail.com>
2018-01-19 19:48:28 -0800

    commit

--- client/state/player/createPlayer.js
+++ client/state/player/createPlayer.js
@@ -1,7 +1,7 @@
 const createPlayer = (x, y, game) => {
   const sprite = game.add.sprite(x, y, 'car');
   game.physics.p2.enable(sprite, false);
-
+  console.log(`BUILD: ${new Date().getMilliseconds}`);
   return sprite;
 }
 
