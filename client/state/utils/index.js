017812ea357132493ca50a5c0dc83f0e4d09faf1
Sondro <memoiran@hotmail.com>
2018-01-19 19:40:32 -0800

    commit

--- client/state/utils/index.js
+++ client/state/utils/index.js
@@ -1,4 +1,5 @@
-export const isDown = (game, key) => game.input.keyboard.isDown(key)
+export const isDown = (game, key) => game.input.keyboard.isDown(key);
+export const isUp = (game, key) => game.input.keyboard.isUp(key);
 export const createText = (game, target) =>
   game.add.text(target.x, target.y, '', {
     fontSize: '12px',
