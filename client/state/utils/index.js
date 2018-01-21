bb1333f8925bbb2fa46e5b540e01b462de93a3c7
Sondro <memoiran@hotmail.com>
2018-01-20 18:45:12 -0800

    commit

--- client/state/utils/index.js
+++ client/state/utils/index.js
@@ -1,18 +1,19 @@
 export const neg = (n) => {
   return 0 - n;
-}
+};
 export const getMPH = (n, s = 0.0833) => {
   if(n < 1) { return 0; }
   return Math.floor(((n * s) * 3600) / 5280);
 };
 export const getMPHstr3 = (n, s = 0.0833) => {
-  if(n < 1) { return 0; }
+  if(n < 1) { return '000 / mph'; }
   return Math.floor(((n * s) * 3600) / 5280).toString().padStart(3, '0') + ' / mph';
 };
 export const getMPHstr4 = (n, s = 0.0833) => {
-  if(n < 1) { return 0; }
+  if(n < 1) { return '0000 / mph'; }
   return Math.floor(((n * s) * 3600) / 5280).toString().padStart(4, '0') + ' / mph';
 };
+
 export const isDown = (game, key) => game.input.keyboard.isDown(key);
 export const createText = (game, target) =>
   game.add.text(target.x, target.y, '', {
