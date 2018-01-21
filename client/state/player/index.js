9e60bfffc06f475b70e7124a635d0dc57f1ec522
Sondro <memoiran@hotmail.com>
2018-01-20 16:02:16 -0800

    commit

--- client/state/player/index.js
+++ client/state/player/index.js
@@ -88,7 +88,7 @@ export default function (x, y, game, socket) {
           y: this.playerName.y
         },
         speed: {
-          value: `$({this.speed / 12) * 3600} / mph`,
+          value: `$(({this.speed / 12) * 3600) / 5280} / mph`,
           x: this.speedText.x,
           y: this.speedText.y
         }
