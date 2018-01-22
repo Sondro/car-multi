3e8b3bf8503f244126f03ce20cbb81eb4cfd89ab
Sondro <memoiran@hotmail.com>
2018-01-22 11:02:09 -0800

    commit

--- client/state/player/index.js
+++ client/state/player/index.js
@@ -4,7 +4,7 @@ export default function (x, y, game, socket) {
   const player = {
     socket,
     sprite: createPlayer(x, y, game),
-    playerName: new Date(years, months, days, housrs, minutes, seconds, miliseconds).toString(),
+    playerName: new Date().toString(),
     playerNum: 7,
     speed: 0,
     speedText: null,
